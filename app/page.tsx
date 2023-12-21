bb1cb2c2d2280defe69d765cb0e7c6d7a8008002
gohab <gohab57432@wlmycn.com>
2023-12-21 13:27:27 -0300

    !feat: recomeçando portfólio do zero

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,9 @@
 "use client";
 
-import React from "react";
-
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Main from "./components/Main";
-
 export default function Home() {
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <div></div>
     </>
   );
 }
